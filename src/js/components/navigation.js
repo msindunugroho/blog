6705b30865ef7e0dd5ceebe7800bef43f9a9b65f
msindunugroho <m.sindunugroho@gmail.com>
2024-05-02 16:02:16 +0700

    add navigation(tablet) component

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,10 +1,9 @@
 const navigation = () => {
-  const menuBtn = document.querySelector('.menu_btn');
   const menu = document.querySelector('.nav_action');
-
-  menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('show_menu');
-  });
+  const menuBtn = document.querySelector('.menu_btn');
+    menuBtn.addEventListener('click', () => {
+      menu.classList.toggle('show_menu');
+    });
 }
 
 export default navigation;
